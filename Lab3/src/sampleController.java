5781e31d8a47a7653bf51e49bf7d663ee1b2e58e
Damian Pękala <damanpekala1@gmail.com>
2021-04-22 08:45:31 +0200

    [Lab3] Zad 5

--- Lab3/src/sampleController.java
+++ Lab3/src/sampleController.java
@@ -1,12 +1,25 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.lang.reflect.Field;
+
 
 public class sampleController {
     public Button btnMain;
     int clickCounter = 0;
+    public TextField txtLicznik;
+
     public void onBtnAction(ActionEvent actionEvent) {
         System.out.println("Kliknięcie nr " + ++clickCounter);
-        if (clickCounter ==1)
+        if (clickCounter == 1)
             btnMain.setText("I jeszcze raz");
+        txtLicznik.setText(String.valueOf(clickCounter));
     }
 }
+
+
+
+
+
+
