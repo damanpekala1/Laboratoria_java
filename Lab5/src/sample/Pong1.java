f27344e7dbbdb59a11c97e7b160ed6817a9b7835
Damian Pękala <damanpekala1@gmail.com>
2021-04-28 16:47:55 +0200

    [Lab5] Zad 1

---
+++ Lab5/src/sample/Pong1.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+
+public class Pong1 extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        stage.setTitle("Kulki");
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+
+        gc.setFill(Color.BLACK);
+        gc.fillRect(50,50,700,500);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
