0e58e20e6320d55e59ff8f08a2ff878440b5cb38
Damian Pękala <damanpekala1@gmail.com>
2021-04-29 17:04:37 +0200

    [Lab5] Zad 9

--- Lab5/src/sample/Pong1.java
+++ Lab5/src/sample/Pong1.java
@@ -56,7 +56,8 @@ public class Pong1 extends Application {
                 lott.nextDouble()*ARENAWIDTH+ARENAX1,
             lott.nextDouble()*ARENAHEIGHT+ARENAY1,
             5+lott.nextDouble()*20,
-            5+lott.nextDouble()*20);
+            5+lott.nextDouble()*20,
+                Color.WHITESMOKE);
         }
 
     private void run(GraphicsContext gc) {
